d8bc5e25d07a339069ac981d0d6cfe1a414a0bd4
Artem Petrishchev <artem.omny@gmail.com>
2023-06-03 12:29:46 +0900

    fix linter problems

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,8 +7,9 @@ const App = () => (
       <img src={logo} className="App-logo" alt="logo" />
       <p>
         Edit
+        {' '}
         <code>src/App.js</code>
-        and save to reload.
+        z and save to reload.
       </p>
       <a
         className="App-link"
