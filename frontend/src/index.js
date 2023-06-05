42c2c4ad52fbebe32a9566d3f772edb989dcc651
Artem Petrishchev <artem.omny@gmail.com>
2023-06-05 13:45:40 +0900

    add routes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App';
+import Login from './Login';
+import NotFound from './NotFound';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './App';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Login />,
+    errorElement: <NotFound />,
+  },
+  {
+    path: 'login',
+    element: <Login />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-// eslint-disable-next-line functional/no-expression-statements
+
 root.render(
   <React.StrictMode>
-    <App />
+    <App>
+      <RouterProvider router={router} />
+    </App>
   </React.StrictMode>,
 );
