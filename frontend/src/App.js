5177f4870bd1eb07bf90f2d1dee28c942dca542b
Artem Petrishchev <artem.omny@gmail.com>
2023-06-05 16:23:27 +0900

    move body elements to index.html

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,20 +4,14 @@ const App = (props) => {
   const { children } = props;
 
   return (
-    <body className="h-100 bg-light">
-      <div className="h-100">
-        <div className="h-100" id="chat">
-          <div className="d-flex flex-column h-100">
-            <nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
-              <div className="container">
-                <a className="navbar-brand" href="/">Hexlet Chat</a>
-              </div>
-            </nav>
-            {children}
-          </div>
+    <div className="d-flex flex-column h-100">
+      <nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
+        <div className="container">
+          <a className="navbar-brand" href="/">Hexlet Chat</a>
         </div>
-      </div>
-    </body>
+      </nav>
+      {children}
+    </div>
   );
 };
 
