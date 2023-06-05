0ce56bac3ea8c5f06b4c65fdd21d5758521db822
Artem Petrishchev <artem.omny@gmail.com>
2023-06-05 23:44:50 +0900

    create Nav element

---
+++ frontend/src/Nav.js
@@ -0,0 +1,9 @@
+const Nav = () => (
+  <nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
+    <div className="container">
+      <a className="navbar-brand" href="/">Hexlet Chat</a>
+    </div>
+  </nav>
+);
+
+export default Nav;
