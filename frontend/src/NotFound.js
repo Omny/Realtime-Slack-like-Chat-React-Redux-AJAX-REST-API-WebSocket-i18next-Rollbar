5aeb09d1d8dcdcf43d66c2ce920a29507a8d1b1d
Artem Petrishchev <artem.omny@gmail.com>
2023-06-05 13:44:51 +0900

    add not found app

---
+++ frontend/src/NotFound.js
@@ -0,0 +1,15 @@
+import notFound from './not-found.svg';
+
+const NotFound = () => (
+  <div className="text-center">
+    <img alt="Страница не найдена" className="img-fluid h-25" src={notFound} />
+    <h1 className="h4 text-muted">Страница не найдена</h1>
+    <p className="text-muted">
+      Но вы можете перейти
+      {' '}
+      <a href="/">на главную страницу</a>
+    </p>
+  </div>
+);
+
+export default NotFound;
