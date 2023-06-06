1560d57cabb4251462ec9cdd28713e6c449cbd4e
Artem Petrishchev <artem.omny@gmail.com>
2023-06-06 22:54:34 +0900

    add userStatus

--- frontend/src/contexts/index.js
+++ frontend/src/contexts/index.js
@@ -1,8 +1,8 @@
 import { createContext } from 'react';
 
 const AppContext = createContext({
-  loggedIn: false,
-  loginError: null,
+  userStatus: null,
+  setUserStatus: () => {},
 });
 
 export default AppContext;
