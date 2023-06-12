1eef9ace14f868871269f740a84ae9e3ec9efe0e
Artem Petrishchev <artem.omny@gmail.com>
2023-06-12 22:34:17 +0900

    remove users slice

--- frontend/src/slices/index.js
+++ frontend/src/slices/index.js
@@ -1,12 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import usersReducer from './usersSlice';
 import channelsReducer from './channelsSlice';
 import messagesReducer from './messagesSlice';
 import setCurrentChannelIdReducer from './currentChannelIdSlice';
 
 const store = configureStore({
   reducer: {
-    users: usersReducer,
     channels: channelsReducer,
     messages: messagesReducer,
     currentChannelId: setCurrentChannelIdReducer,
