ce9a4e676b4ffcbf6296f36ebcf34deb9c1ff886
Artem Petrishchev <artem.omny@gmail.com>
2023-06-13 22:37:44 +0900

    move App to App.js

---
+++ frontend/src/components/App.js
@@ -0,0 +1,53 @@
+import { useState, useContext } from 'react';
+import {
+  BrowserRouter as Router, Route, Routes, Navigate,
+} from 'react-router-dom';
+
+import { Provider } from 'react-redux';
+import store from '../slices';
+import AppContext from '../contexts';
+
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Nav from './Nav';
+import Login from './Login';
+import NotFound from './NotFound';
+import Chat from './Chat';
+
+const AppProvider = ({ children }) => {
+  const token = localStorage.getItem('token');
+  const [userGroup, setUserGroup] = useState(token ? 'user' : 'guest');
+
+  return (
+    // eslint-disable-next-line react/jsx-no-constructed-context-values
+    <AppContext.Provider value={{
+      userGroup, setUserGroup,
+    }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+const ChatOrLogin = () => {
+  const { userGroup } = useContext(AppContext);
+  return userGroup === 'user' ? <Chat /> : <Navigate to="/login" />;
+};
+
+const App = () => (
+  <Provider store={store}>
+    <AppProvider>
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<ChatOrLogin />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </AppProvider>
+  </Provider>
+);
+
+export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,55 +1,12 @@
-import React, { useState, useContext } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {
-  BrowserRouter as Router, Route, Routes, Navigate,
-} from 'react-router-dom';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { Provider } from 'react-redux';
-import store from './slices';
-import AppContext from './contexts';
-
-import Nav from './components/Nav';
-import Login from './components/Login';
-import NotFound from './components/NotFound';
-import Chat from './components/Chat';
-
-const AppProvider = ({ children }) => {
-  const token = localStorage.getItem('token');
-  const [userGroup, setUserGroup] = useState(token ? 'user' : 'guest');
-
-  return (
-    // eslint-disable-next-line react/jsx-no-constructed-context-values
-    <AppContext.Provider value={{
-      userGroup, setUserGroup,
-    }}
-    >
-      {children}
-    </AppContext.Provider>
-  );
-};
-
-const ChatOrLogin = () => {
-  const { userGroup } = useContext(AppContext);
-  return userGroup === 'user' ? <Chat /> : <Navigate to="/login" />;
-};
+import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <AppProvider>
-        <Router>
-          <Nav />
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/" element={<ChatOrLogin />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Router>
-      </AppProvider>
-    </Provider>
+    <App />
   </React.StrictMode>,
 );
