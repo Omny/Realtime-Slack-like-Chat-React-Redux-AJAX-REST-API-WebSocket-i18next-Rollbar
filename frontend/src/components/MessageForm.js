8253f45c4e36aa4da36efa76f85e26a81126b271
Artem Petrishchev <artem.omny@gmail.com>
2023-06-16 19:11:29 +0900

    fix empty form

--- frontend/src/components/MessageForm.js
+++ frontend/src/components/MessageForm.js
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { ArrowRightSquare } from 'react-bootstrap-icons';
 import { Formik, Form, Field } from 'formik';
@@ -9,7 +8,6 @@ const MessageForm = () => {
   const dispatch = useDispatch();
   const currentChannelId = useSelector((state) => state.currentChannelId);
   const username = localStorage.getItem('user');
-  const [isFormEmpty, setIsFormEmpty] = useState(true);
 
   return (
     <Formik
@@ -19,30 +17,33 @@ const MessageForm = () => {
         dispatch(sendNewMessage({ body, channelId: currentChannelId, username }));
         resetForm();
         setSubmitting(false);
-        setIsFormEmpty(true);
       }}
       validate={(values) => {
         const errors = {};
         if (!values.body) {
           errors.body = 'Поле обязательно для заполнения';
         }
-        setIsFormEmpty(Object.keys(values).length === 0);
         return errors;
       }}
     >
-      {({ errors, isSubmitting }) => (
+      {({ errors, values, isSubmitting }) => (
         <Form noValidate className="py-1 border rounded-2">
-          <div className={cn('input-group', { 'has-validation': isFormEmpty || errors.body || isSubmitting })}>
+          <div className={cn('input-group', { 'has-validation': !values.body || errors.body || isSubmitting })}>
             <Field
               type="text"
               name="body"
+              value={values.body}
               aria-label="Новое сообщение"
               placeholder="Введите сообщение…"
               className="border-0 p-0 ps-2 form-control"
               data-last-active-input
               autoFocus
             />
-            <button type="submit" disabled={isFormEmpty || errors.body || isSubmitting} className="btn btn-group-vertical">
+            <button
+              type="submit"
+              disabled={!values.body || errors.body || isSubmitting}
+              className="btn btn-primary btn-group-vertical"
+            >
               <ArrowRightSquare size={20} />
               <span className="visually-hidden">Отправить</span>
             </button>
