94b5df713e4a5f24da838015130a7938a9eea05e
Artem Petrishchev <artem.omny@gmail.com>
2023-06-23 17:18:20 +0900

    move contect to separete file

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,13 +1,9 @@
-import {
-  useState, useContext, useCallback, useMemo,
-} from 'react';
-import {
-  BrowserRouter as Router, Route, Routes, Navigate, useNavigate,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import '../i18n';
 
 import { Provider } from 'react-redux';
 import store from '../slices';
-import AuthContext from '../contexts';
+import AuthProvider from '../contexts/AuthProvider';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -15,47 +11,7 @@ import Nav from './Nav';
 import Login from './Login';
 import Signup from './Signup';
 import NotFound from './NotFound';
-import Chat from './Chat';
-
-const AuthProvider = ({ children }) => {
-  const navigate = useNavigate();
-  const [userGroup, setUserGroup] = useState(localStorage.getItem('token')
-    ? 'user' : 'guest');
-
-  const handleLogout = useCallback(() => {
-    localStorage.clear();
-    setUserGroup('guest');
-  }, []);
-
-  const handleLogin = useCallback((user, token) => {
-    localStorage.setItem('user', user);
-    localStorage.setItem('token', token);
-    setUserGroup('user');
-    navigate('/');
-  }, [navigate]);
-
-  const authContextValue = useMemo(
-    () => ({
-      userGroup,
-      handleLogin,
-      handleLogout,
-    }),
-    [userGroup, handleLogin, handleLogout],
-  );
-
-  return (
-    <AuthContext.Provider
-      value={authContextValue}
-    >
-      {children}
-    </AuthContext.Provider>
-  );
-};
-
-const ChatOrLogin = () => {
-  const { userGroup } = useContext(AuthContext);
-  return userGroup === 'user' ? <Chat /> : <Navigate to="/login" replace />;
-};
+import ChatOrLogin from './ChatOrLogin';
 
 const App = () => (
   <Router>
