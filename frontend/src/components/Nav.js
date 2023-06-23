1ad7d0a434be2ab01c6865f0779de251e54bd44c
Artem Petrishchev <artem.omny@gmail.com>
2023-06-23 15:57:38 +0900

    make working translation

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -1,23 +1,25 @@
 import { useContext } from 'react';
 import { Button } from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
 import AuthContext from '../contexts';
 
 const Nav = () => {
+  const { t } = useTranslation();
   const { userGroup, handleLogout } = useContext(AuthContext);
   const username = localStorage.getItem('user');
 
   return (
     <nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
       <div className="container">
-        <a className="navbar-brand" href="/">Hexlet Chat</a>
+        <a className="navbar-brand" href="/">{t('hexletChat')}</a>
         {userGroup === 'user' ? (
           <>
             <span className="navbar-text">
-              Добро пожаловать,
-              {' '}
+              {t('welcome')}
+              {', '}
               {username}
             </span>
-            <Button variant="outline-primary" onClick={handleLogout}>Выйти</Button>
+            <Button variant="outline-primary" onClick={handleLogout}>{t('logout')}</Button>
           </>
         ) : null}
       </div>
