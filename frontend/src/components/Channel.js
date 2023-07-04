359959b092ddae3b6b9d0d8145661e2e3563086b
Artem Petrishchev <artem.omny@gmail.com>
2023-07-04 23:18:27 +0900

    split Channel and Channels

---
+++ frontend/src/components/Channel.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useRef } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import cn from 'classnames';
+import { useTranslation } from 'react-i18next';
+import Button from 'react-bootstrap/Button';
+import Dropdown from 'react-bootstrap/Dropdown';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import { setCurrentChannelId } from '../slices/currentChannelIdSlice';
+import { openModal } from '../slices/modalSlice';
+
+const Channel = ({ id, name, removable }) => {
+  const { t } = useTranslation();
+  const dispatch = useDispatch();
+  const currentChannelId = useSelector((state) => state.currentChannelId);
+
+  const handleShowRemoveChannelModal = (clickedId) => {
+    dispatch(openModal({ modalType: 'removeChannel', idToProcess: clickedId }));
+  };
+
+  const handleShowRenameChannelModal = (clickedId) => {
+    dispatch(openModal({ modalType: 'renameChannel', idToProcess: clickedId }));
+  };
+
+  const handleClickOnChannel = (clickedId) => {
+    dispatch(setCurrentChannelId(clickedId));
+  };
+
+  const currentChannelEl = useRef(null);
+  useEffect(() => {
+    if (currentChannelEl.current) {
+      currentChannelEl.current.scrollIntoView({ behavior: 'auto' });
+    }
+  }, []);
+
+  const isCurrentChannel = id === currentChannelId;
+  const btnClassName = cn('w-100 rounded-0 text-start text-truncate', { 'btn-secondary': isCurrentChannel });
+  const btnVariant = isCurrentChannel ? 'secondary' : 'light';
+
+  return removable ? (
+    <Dropdown ref={isCurrentChannel ? currentChannelEl : null} as={ButtonGroup} className="d-flex dropdown btn-group">
+      <Button type="button" variant={btnVariant} className={btnClassName} onClick={() => handleClickOnChannel(id)}>
+        <span className="me-1">#</span>
+        {name}
+      </Button>
+      <Dropdown.Toggle split variant={btnVariant} id="dropdown-split-basic">
+        <span className="visually-hidden">{t('channels.menu')}</span>
+      </Dropdown.Toggle>
+      <Dropdown.Menu title="" variant={btnVariant} id="bg-nested-dropdown">
+        <Dropdown.Item onClick={() => handleShowRemoveChannelModal(id)} eventKey="1">
+          {t('channels.remove')}
+        </Dropdown.Item>
+        <Dropdown.Item onClick={() => handleShowRenameChannelModal(id)} eventKey="2">
+          {t('channels.rename')}
+        </Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  ) : (
+    <Button ref={isCurrentChannel ? currentChannelEl : null} variant={btnVariant} type="button" className={btnClassName} onClick={() => handleClickOnChannel(id)}>
+      <span className="me-1">#</span>
+      {name}
+    </Button>
+  );
+};
+
+export default Channel;
--- frontend/src/components/Channels.js
+++ frontend/src/components/Channels.js
@@ -1,41 +1,19 @@
 import { useSelector, useDispatch } from 'react-redux';
-import cn from 'classnames';
 import { useTranslation } from 'react-i18next';
 import { Plus } from 'react-bootstrap-icons';
 import Button from 'react-bootstrap/Button';
-import Dropdown from 'react-bootstrap/Dropdown';
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
-import { useEffect, useRef } from 'react';
 import { selectors as channelsSelectors } from '../slices/channelsSlice';
-import { setCurrentChannelId } from '../slices/currentChannelIdSlice';
 import { openModal } from '../slices/modalSlice';
+import Channel from './Channel';
 
 const Channels = () => {
   const { t } = useTranslation();
   const dispatch = useDispatch();
-
   const channels = useSelector(channelsSelectors.selectAll);
-  const currentChannelId = useSelector((state) => state.currentChannelId);
-  const handleClickOnChannel = (selectedId) => {
-    dispatch(setCurrentChannelId(selectedId));
-  };
 
   const handleShowAddChannelModal = () => {
     dispatch(openModal({ modalType: 'addChannel' }));
   };
-  const handleShowRemoveChannelModal = (id) => {
-    dispatch(openModal({ modalType: 'removeChannel', idToProcess: id }));
-  };
-  const handleShowRenameChannelModal = (id) => {
-    dispatch(openModal({ modalType: 'renameChannel', idToProcess: id }));
-  };
-
-  const currentChannelEl = useRef(null);
-  useEffect(() => {
-    if (currentChannelEl.current) {
-      currentChannelEl.current.scrollIntoView({ behavior: 'auto' });
-    }
-  }, [channels]);
 
   return (
     <div className="col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex">
@@ -48,35 +26,14 @@ const Channels = () => {
       </div>
       <ul id="channels-box" className="nav flex-column nav-pills nav-fill px-2 mb-3 overflow-auto h-100 d-block">
         <li className="nav-item w-100">
-          {channels.map(({ id, name, removable }) => {
-            const isCurrentChannel = id === currentChannelId;
-            const btnClassName = cn('w-100 rounded-0 text-start text-truncate', { 'btn-secondary': isCurrentChannel });
-            const btnVariant = (isCurrentChannel) ? 'secondary' : 'light';
-            return removable ? (
-              <Dropdown ref={isCurrentChannel ? currentChannelEl : null} as={ButtonGroup} key={id} className="d-flex dropdown btn-group">
-                <Button type="button" variant={btnVariant} className={btnClassName} onClick={() => handleClickOnChannel(id)}>
-                  <span className="me-1">#</span>
-                  {name}
-                </Button>
-                <Dropdown.Toggle split variant={btnVariant} id="dropdown-split-basic">
-                  <span className="visually-hidden">{t('channels.menu')}</span>
-                </Dropdown.Toggle>
-                <Dropdown.Menu title="" variant={btnVariant} id="bg-nested-dropdown">
-                  <Dropdown.Item onClick={() => handleShowRemoveChannelModal(id)} eventKey="1">
-                    {t('channels.remove')}
-                  </Dropdown.Item>
-                  <Dropdown.Item onClick={() => handleShowRenameChannelModal(id)} eventKey="2">
-                    {t('channels.rename')}
-                  </Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
-            ) : (
-              <Button ref={isCurrentChannel ? currentChannelEl : null} variant={btnVariant} key={id} type="button" className={btnClassName} onClick={() => handleClickOnChannel(id)}>
-                <span className="me-1">#</span>
-                {name}
-              </Button>
-            );
-          })}
+          {channels.map(({ id, name, removable }) => (
+            <Channel
+              key={id}
+              id={id}
+              name={name}
+              removable={removable}
+            />
+          ))}
         </li>
       </ul>
     </div>
