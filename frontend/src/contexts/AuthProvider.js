bf63935d95ecba48080abead65dec71cc411f5f7
Artem Petrishchev <artem.omny@gmail.com>
2023-07-10 11:09:35 +0900

    remove user groups

--- frontend/src/contexts/AuthProvider.js
+++ frontend/src/contexts/AuthProvider.js
@@ -1,24 +1,20 @@
-import { useState, useCallback, useMemo } from 'react';
+import { useCallback, useMemo } from 'react';
 import { useNavigate } from 'react-router-dom';
 import AuthContext from '.';
 import routes from '../routes';
 
 const AuthProvider = ({ children }) => {
   const navigate = useNavigate();
-  const [userGroup, setUserGroup] = useState(localStorage.getItem('token')
-    ? 'user' : 'guest');
 
   const handleLogin = useCallback((user, token) => {
     localStorage.setItem('user', user);
     localStorage.setItem('token', token);
-    setUserGroup('user');
     navigate(routes.homePagePath());
   }, [navigate]);
 
   const handleLogout = useCallback(() => {
     localStorage.removeItem('user');
     localStorage.removeItem('token');
-    setUserGroup('guest');
     navigate(routes.loginPagePath());
   }, [navigate]);
 
@@ -28,9 +24,9 @@ const AuthProvider = ({ children }) => {
 
   const authContextValue = useMemo(
     () => ({
-      handleLogin, handleLogout, getToken, getUsername, userGroup,
+      handleLogin, handleLogout, getToken, getUsername,
     }),
-    [handleLogin, handleLogout, getToken, getUsername, userGroup],
+    [handleLogin, handleLogout, getToken, getUsername],
   );
 
   return (
--- frontend/src/contexts/index.js
+++ frontend/src/contexts/index.js
@@ -1,8 +1,5 @@
 import { createContext } from 'react';
 
-const AuthContext = createContext({
-  userGroup: null,
-  setUserGroup: () => {},
-});
+const AuthContext = createContext({});
 
 export default AuthContext;
