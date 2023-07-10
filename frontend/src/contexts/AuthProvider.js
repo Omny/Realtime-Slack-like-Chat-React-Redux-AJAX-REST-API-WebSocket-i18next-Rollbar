a7b1190ce74a3dc6b57dadf509312f674a3c4ce3
Artem Petrishchev <artem.omny@gmail.com>
2023-07-10 11:23:39 +0900

    add loggedIn state

--- frontend/src/contexts/AuthProvider.js
+++ frontend/src/contexts/AuthProvider.js
@@ -1,20 +1,29 @@
-import { useCallback, useMemo } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import AuthContext from '.';
 import routes from '../routes';
 
 const AuthProvider = ({ children }) => {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  //
   const navigate = useNavigate();
 
   const handleLogin = useCallback((user, token) => {
     localStorage.setItem('user', user);
     localStorage.setItem('token', token);
+    setLoggedIn(true);
+
+    //
     navigate(routes.homePagePath());
   }, [navigate]);
 
   const handleLogout = useCallback(() => {
     localStorage.removeItem('user');
     localStorage.removeItem('token');
+    setLoggedIn(false);
+
+    //
     navigate(routes.loginPagePath());
   }, [navigate]);
 
@@ -24,9 +33,9 @@ const AuthProvider = ({ children }) => {
 
   const authContextValue = useMemo(
     () => ({
-      handleLogin, handleLogout, getToken, getUsername,
+      loggedIn, handleLogin, handleLogout, getToken, getUsername,
     }),
-    [handleLogin, handleLogout, getToken, getUsername],
+    [loggedIn, handleLogin, handleLogout, getToken, getUsername],
   );
 
   return (
