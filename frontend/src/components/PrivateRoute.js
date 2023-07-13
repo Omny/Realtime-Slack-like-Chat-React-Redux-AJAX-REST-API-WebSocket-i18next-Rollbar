7f4ac6485de86afa742521d094ad8250d431ccc4
Artem Petrishchev <artem.omny@gmail.com>
2023-07-13 10:01:32 +0900

    fix path from routes

--- frontend/src/components/PrivateRoute.js
+++ frontend/src/components/PrivateRoute.js
@@ -1,9 +1,10 @@
 import { Navigate } from 'react-router-dom';
 import useAuth from '../hooks';
+import routes from '../routes';
 
 const PrivateRoute = ({ children }) => {
   const auth = useAuth();
-  return auth.isLoggedIn ? children : <Navigate to="/login" replace />;
+  return auth.isLoggedIn ? children : <Navigate to={routes.loginPagePath()} replace />;
 };
 
 export default PrivateRoute;
