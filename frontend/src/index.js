73d5bc5f84a8317a332a420c036066463c12ebe9
Artem Petrishchev <artem.omny@gmail.com>
2023-07-13 09:20:01 +0900

    move css imports to index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './components/App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 const root = createRoot(document.getElementById('root'));
 
