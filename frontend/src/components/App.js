3e49afcde65b55a7e9604afd08cc46eacca59172
Artem Petrishchev <artem.omny@gmail.com>
2023-07-18 11:39:28 +0900

    move provider to init

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,8 +2,7 @@ import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { ErrorBoundary } from '@rollbar/react';
 import { ToastContainer } from 'react-toastify';
-import { Provider } from 'react-redux';
-import store from '../slices';
+import { useDispatch } from 'react-redux';
 import AuthProvider from '../contexts/AuthProvider';
 import socketManager from '../socketManager';
 import { newMessage } from '../slices/messagesSlice';
@@ -18,36 +17,36 @@ import PrivateRoute from './PrivateRoute';
 import ErrorDisplay from './ErrorDisplay';
 
 const App = () => {
+  const dispatch = useDispatch();
+
   useEffect(() => {
-    socketManager.subscribe('newMessage', (payload) => { store.dispatch(newMessage(payload)); });
-    socketManager.subscribe('newChannel', (payload) => { store.dispatch(newChannel(payload)); });
-    socketManager.subscribe('removeChannel', (payload) => { store.dispatch(removeChannel(payload.id)); });
-    socketManager.subscribe('renameChannel', (payload) => { store.dispatch(renameChannel(payload)); });
-  }, []);
+    socketManager.subscribe('newMessage', (payload) => { dispatch(newMessage(payload)); });
+    socketManager.subscribe('newChannel', (payload) => { dispatch(newChannel(payload)); });
+    socketManager.subscribe('removeChannel', (payload) => { dispatch(removeChannel(payload.id)); });
+    socketManager.subscribe('renameChannel', (payload) => { dispatch(renameChannel(payload)); });
+  }, [dispatch]);
 
   return (
     <div className="d-flex flex-column h-100">
       <ErrorBoundary fallbackUI={ErrorDisplay}>
         <Router>
-          <Provider store={store}>
-            <AuthProvider>
-              <Nav />
-              <Routes>
-                <Route path={routes.loginPagePath()} element={<Login />} />
-                <Route path={routes.signupPagePath()} element={<Signup />} />
-                <Route
-                  path={routes.homePagePath()}
-                  element={(
-                    <PrivateRoute>
-                      <Chat />
-                    </PrivateRoute>
-                  )}
-                />
-                <Route path="*" element={<NotFound />} />
-              </Routes>
-              <ToastContainer />
-            </AuthProvider>
-          </Provider>
+          <AuthProvider>
+            <Nav />
+            <Routes>
+              <Route path={routes.loginPagePath()} element={<Login />} />
+              <Route path={routes.signupPagePath()} element={<Signup />} />
+              <Route
+                path={routes.homePagePath()}
+                element={(
+                  <PrivateRoute>
+                    <Chat />
+                  </PrivateRoute>
+                )}
+              />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+            <ToastContainer />
+          </AuthProvider>
         </Router>
       </ErrorBoundary>
     </div>
