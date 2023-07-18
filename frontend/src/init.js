1ed6629054b8a137d0ad0de449defc3117a4fc86
Artem Petrishchev <artem.omny@gmail.com>
2023-07-18 11:39:42 +0900

    move provider to init

--- frontend/src/init.js
+++ frontend/src/init.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { Provider as RollbarProvider } from '@rollbar/react';
 import i18next from 'i18next';
 import { I18nextProvider, initReactI18next } from 'react-i18next';
 import filter from 'leo-profanity';
 import resources from './locales/index.js';
+// import socketManager from '../socketManager';
+import store from './slices';
 import App from './components/App';
 
 const i18n = i18next.createInstance();
@@ -31,7 +34,9 @@ filter.add(filter.getDictionary('ru'));
 export default async () => (
   <RollbarProvider config={rollbarConfig}>
     <I18nextProvider i18n={i18n}>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </I18nextProvider>
   </RollbarProvider>
 );
