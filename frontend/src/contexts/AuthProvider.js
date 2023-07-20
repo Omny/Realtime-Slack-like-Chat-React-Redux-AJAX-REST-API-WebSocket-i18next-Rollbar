b5adc6b3d2341280e040d957f1a926f01a46f7d7
Artem Petrishchev <artem.omny@gmail.com>
2023-07-20 19:37:10 +0900

    fix users in two windows must write at the same time

--- frontend/src/contexts/AuthProvider.js
+++ frontend/src/contexts/AuthProvider.js
@@ -2,23 +2,26 @@ import { useCallback, useMemo, useState } from 'react';
 import { AuthContext } from '.';
 
 const AuthProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(localStorage.getItem('user') || null);
   const [isLoggedIn, setLoggedIn] = useState(false);
 
   const handleLogin = useCallback((user, token) => {
     localStorage.setItem('user', user);
     localStorage.setItem('token', token);
+    setCurrentUser(user);
     setLoggedIn(true);
   }, []);
 
   const handleLogout = useCallback(() => {
     localStorage.removeItem('user');
     localStorage.removeItem('token');
+    setCurrentUser(null);
     setLoggedIn(false);
   }, []);
 
   const getToken = useCallback(() => localStorage.getItem('token'), []);
 
-  const getUsername = useCallback(() => localStorage.getItem('user'), []);
+  const getUsername = useCallback(() => currentUser, [currentUser]);
 
   const authContextValue = useMemo(
     () => ({
